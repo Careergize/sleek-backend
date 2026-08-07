7630d8f75dba6abf0960c778a4b08db4afbc1677
NidhinGJoseph <nidhinjoseph1331@gmail.com>
2026-08-08 00:45:10 +0530

    read DEBUG from env var

--- sleekbackend/settings.py
+++ sleekbackend/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#msu^1=o+nw4xr0u=ea1z1b#xsw%d2m%4hed15h!dh-i&gvs_a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = [
     "sleek-backend-dghhanbpeqcyfhhw.centralindia-01.azurewebsites.net",
